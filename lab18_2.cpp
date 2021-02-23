64b388047874368b12a462c075cd0187d58719f6
fordsaranpong <76035396+fordsaranpong@users.noreply.github.com>
2021-02-23 17:51:17 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
+#include<cmath>
+
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
-};
+struct Rect{double x,y,w,h;};
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2)
+{
+  double L1= min(R1->x+R1->w,R2->x+R2->w) - max(R1->x, R2->x);
+    double L2 = min(R1->y, R2->y) - max(R1->y-R1->h, R2->y-R2->h);
 
-
-
+    if( L1 < 0|| L2 < 0 )
+    {
+      return 0;
+    }
+    else {	
+  return  L1 * L2;
+	}
 }
+ 
+
